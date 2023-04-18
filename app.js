b2d3700fd715387bd59417417d2dc0f25b3ddef8
ThaksinCK <thaksin.ckn@gmail.com>
2023-04-18 20:56:23 +0700

    add database mongodb

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
-const body = require("body-parser");
 const bodyParser = require("body-parser");
 const port = 8080;
+const mongoose = require("mongoose");
 const app = express();
 
 var items = [];
@@ -10,6 +10,28 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(express.static('public'));
 
+mongoose.connect("mongodb://127.0.0.1:27017/todolistDB");
+
+const itemSchema = new mongoose.Schema({
+    item : String
+});
+
+const Item = mongoose.model("todolistItem", itemSchema);
+
+const item1 = new Item({
+    item : "Welcom to TodoList website"
+});
+
+const item2 = new Item({
+    item : "Hit the + to add your new list"
+});
+
+const item3 = new Item({
+    item : "Hit the checkboc to delete your list"
+});
+
+const defaultItems = [item1, item2, item3];
+
 app.get("/", (req, res)=>{
     const today = new Date();
     const options = {
@@ -17,16 +39,29 @@ app.get("/", (req, res)=>{
         month : "long",
         day : "numeric"
     };
-
-    res.render("index", {ThisDay : today.toLocaleDateString("en-US", options), newListItem : items});
+    Item.find({}).then((value) => {
+        if(value.length === 0){
+            Item.insertMany(defaultItems);
+        }
+        res.render("index", {ThisDay : today.toLocaleDateString("en-US", options), newListItem : value});
+    })
+    
 });
 
 app.post("/", (req, res)=>{
-    var item = req.body.newList;
-    items.push(item);
+    var item = new Item({
+        item : req.body.newList
+    })
+    item.save()
     res.redirect("/");
 })
 
+app.post("/delete", (req, res)=>{
+    const itemId = req.body.checkbox;
+    Item.deleteOne({ _id : itemId}).then(() =>{
+        res.redirect("/");
+    })
+})
 app.listen(port, ()=>{
     console.log(`server started at port ${port}`);
 });
